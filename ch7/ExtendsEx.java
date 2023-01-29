1c57d02fb2ad49e428b614bdfe0dd01d738993aa
busungg <busungg@naver.com>
2023-01-29 22:32:31 +0900

    자바 공부

---
+++ ch7/ExtendsEx.java
@@ -0,0 +1,20 @@
+package ch7;
+
+class Parent {
+    int age;
+    int weight;
+
+    int info() {
+        return 1;
+    }
+}
+
+class Child extends Parent{
+    String name;
+}
+
+class ExtendsEx {
+    public static void main(String[] args) {
+        
+    }
+}
